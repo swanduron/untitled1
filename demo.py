c0e746b677d6e3c5eed0b60d044de3f7639bc3f9
haitao <swanduron@hotmail.com>
2021-02-03 14:27:53 +0800

    no idea

--- demo.py
+++ demo.py
@@ -24,18 +24,14 @@ class Ts_engine():
         self.ts = ts
 
     def get_price(self, date, ts_code):
-        time.sleep(0.5)
-        # dt_obj = datetime.datetime.strptime(date, '%Y%m%d')
-        # delta = datetime.timedelta(days=1)
-        # n_days = dt_obj + delta
+        time.sleep(0.2)
         df = self.pro.daily(trade_date=date, ts_code=str(ts_code))
-        # print('trade_date=',date, '<=>ts_code=',str(ts_code), '<=>date=', date)
         try:
             ts_code, trade_date, open_price, high, low, close_price, pre_close, change, pct_chg, vol, amount = df.values[0]
-            return open_price, close_price
+            return open_price, close_price, vol
         except Exception as e:
-            print ('No exchange date for [%s] in [%s]' % (ts_code, date))
-            return 0,0
+            # print ('No exchange date for [%s] in [%s]' % (ts_code, date))
+            return 0, 0, vol
 
 class Stock():
 
@@ -43,27 +39,34 @@ class Stock():
         self.id, self.location = ts_code.split('.')
         self.init_date = init_date
         self.total_operation = 1
-        self.total_value = amount
+        self.total_value = 0
         self.amount = amount
         self.price = price
         self.income_flag = True
         self.outgoing_flag = False
+        self.realtime_price = 0
         self.last_op_data = ''
+        self.clear_flag = False
+        self.reason = ''
+        self.vol = 0
 
     def ts_code(self):
         return ('.'.join([self.id, self.location]))
 
     def add(self, amount, price):
         self.total_operation += 1
-        self.total_value += abs(amount)
+        self.total_value += amount * price
         self.amount += amount
         self.price = price
+        self.realtime_price = round(self.total_value / self.amount, 3)
 
     def reduce(self, amount, price):
         self.total_operation -= 1
-        self.total_value += abs(amount)
+        self.total_value -= amount * price
         self.amount -= amount
         self.price = price
+        if self.amount != 0:
+            self.realtime_price = round(self.total_value / self.amount, 3)
 
     def __repr__(self):
         return ('.'.join([self.id, self.location]))
@@ -85,61 +88,97 @@ def list_check(live_stock, date):
             loop_counter = 0
             while True:
                 backup_stock = random.choice(stock_symbols)
-                print('Attempt stock [%s]  ' % backup_stock, end='')
-                open_price, close_price = ts.get_price(BASE_DATE, backup_stock)
-                print('Parse stock [%s] on [%s]   ' % (backup_stock, BASE_DATE), end='')
-                if open_price != 0 and close_price != 0:
+                # print('Attempt stock [%s]  ' % backup_stock)
+                open_price, close_price, vol = ts.get_price(BASE_DATE, backup_stock)
+                # print('Parse stock [%s] on [%s]   ' % (backup_stock, BASE_DATE))
+                if open_price != 0 and close_price != 0 and close_price > 20:
                     stock = Stock(backup_stock, BASE_DATE, 0, 0)
+                    stock.reason = '新加票'
                     clear_list.append(stock)
                     print('Add new stock in tracing list [%s]' % backup_stock)
                     break
+                elif open_price != 0 and close_price != 0 and close_price <= 20:
+                    # print('Mismatch price < %s, current price %s' % (20, close_price))
+                    loop_counter = 0
+                    continue
                 else:
-                    print('No trade date is available for stock [%s]' % backup_stock)
+                    # print('No trade date is available for stock [%s] on certain value [%s]' % (backup_stock, close_price))
                     loop_counter += 1
-                    print('loop_counter>', loop_counter)
+                    # print('loop_counter>', loop_counter)
                     if loop_counter >= 3:
                         dt_obj = datetime.datetime.strptime(BASE_DATE, '%Y%m%d')
                         delta = datetime.timedelta(days=1)
                         BASE_DATE = dt_obj + delta
-                        print('MID BASE_DATE>>', BASE_DATE)
+                        # print('MID BASE_DATE>>', BASE_DATE)
                         BASE_DATE = BASE_DATE.strftime('%Y%m%d')
                         loop_counter = 0
     return clear_list
 
 ts = Ts_engine()
-#
-# for stock in selected_stock:
-#     open_price, close_price = ts.get_price(BASE_DATE, stock)
-#     stock_obj = Stock(stock, BASE_DATE, 100, open_price)
-#     money -= open_price * 100
-#     live_stock.append(stock)
-
 
 for i in range(300):
-    for i in live_stock:
-        print('<%s - %s> ' % (i.ts_code(), i.amount), end='')
-    print('')
+
     for stock in live_stock:
-        open_price, close_price = ts.get_price(BASE_DATE, stock)
-        if open_price != 0:
-            if stock.income_flag == True:
+        open_price, close_price, vol = ts.get_price(BASE_DATE, stock)
+        if open_price != 0 and not stock.clear_flag:
+            stock.price = close_price
+            if stock.income_flag:
+                if money < open_price * 100:
+                    stock.income_flag = False
+                    continue
                 stock.add(100, open_price)
-                stock.price = open_price
                 money -= open_price * 100
+                money -= open_price * 100 * 0.0005
                 stock.income_flag = False
                 stock.last_op_data = BASE_DATE
-                print('Execute ADD OPERATION')
-            if stock.outgoing_flag == True:
+                # print('Execute ADD OPERATION on [%s] - %s' % (stock.ts_code(), stock.reason))
+                stock.reason = ''
+            if stock.outgoing_flag:
                 stock.reduce(100, open_price)
                 money += open_price * 100
+                money -= open_price * 100 * 0.0005
                 stock.price = open_price
                 stock.outgoing_flag = False
                 stock.last_op_data = BASE_DATE
-                print('Execute REDUCE OPERATION')
-            if (close_price - stock.price)/stock.price >= 0.03 and stock.init_date != BASE_DATE:
+                # print('Execute REDUCE OPERATION on [%s] - %s' % (stock.ts_code(), stock.reason))
+                stock.reason = ''
+
+            # if (close_price - stock.realtime_price)/stock.realtime_price >= 0.03 and stock.init_date != BASE_DATE:
+            #     stock.income_flag = True
+            # elif (close_price - stock.realtime_price)/stock.realtime_price <= -0.03 and stock.init_date != BASE_DATE:
+            #     stock.outgoing_flag = True
+
+            # Only concern price on each exchange day
+            if 0.05 >= (close_price - open_price)/open_price >= 0.03:
                 stock.income_flag = True
-            elif (close_price - stock.price)/stock.price <= -0.03 and stock.init_date != BASE_DATE:
+                stock.reason = '持仓，股价当日上涨3%'
+                print('预备加仓，股价当日上涨3 open-%s  close-%s' % (open_price, close_price))
+            # elif -0.05 <= (close_price - open_price)/open_price <= -0.03:
+            #     stock.outgoing_flag = True
+            #     stock.reason = '减仓，股价当日下跌3%但是小于5%'
+            elif (close_price - open_price)/open_price < -0.05:
+                stock.clear_flag = True
+                stock.reason = '斩仓，当日股价下跌5%'
+                print('预备斩仓，当日股价下跌5 open-%s  close-%s' % (open_price, close_price))
+            elif (stock.realtime_price - close_price)/stock.realtime_price > 0.3:
+                stock.clear_flag = True
+                stock.reason = '斩仓，盈利30%'
+                print('预备斩仓，盈利30 rt_price-%s  close-%s' % (stock.realtime_price, close_price))
+            elif (close_price - stock.realtime_price)/stock.realtime_price < -0.03:
                 stock.outgoing_flag = True
+                stock.reason = '减仓，股价浮亏3'
+                print('预备减仓，股价浮亏3  rt_price-%s  cloes-%s' % (stock.realtime_price, close_price))
+
+        elif stock.clear_flag:
+            money += open_price * stock.amount
+            stock.price = open_price
+            stock.amount = 0
+            print('Execute CLEAR OPERATION on [%s] - %s' % (stock.ts_code(), stock.reason))
+
+    for i in live_stock:
+        print('<%s - %s - %s - %s> ' % (i.ts_code(), i.amount, i.realtime_price, i.price), end='')
+    print('')
+
     live_stock = list_check(live_stock, BASE_DATE)
 
     dt_obj = datetime.datetime.strptime(BASE_DATE, '%Y%m%d')
@@ -150,3 +189,10 @@ for i in range(300):
     BASE_DATE = dt_obj + delta
     BASE_DATE = BASE_DATE.strftime('%Y%m%d')
     print('MONEY is [%s]  Date is [%s]' % (money, BASE_DATE))
+
+print('FUNCTION DOWN.')
+for stock in live_stock:
+    money += stock.total_value
+    print('Stock [%s] has value: [%s]' % (stock.ts_code(), stock.total_value))
+
+print('You Have [%s] in the end of this loop.'% money)
